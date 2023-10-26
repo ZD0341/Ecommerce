0238bad7b9d196166662522928ead0d4da09ad6b
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-26 15:17:09 -0700

    modelroutes

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,44 @@
+// Import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// Import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
+class ProductTag extends Model {}
+
+// Set up fields and rules for ProductTag model
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
